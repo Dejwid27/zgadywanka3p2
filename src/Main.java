3fc905e6ad7d5d4d7c81c8d0b1cc770c1bbb9e31
Dejwid27 <dawidpiejjo@gmail.com>
2024-09-06 10:12:53 +0200

    int losowanie

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+        int wylosowanaLiczba = (int) (Math.random()*100+1);
+        System.out.println(wylosowanaLiczba);
     }
 }
