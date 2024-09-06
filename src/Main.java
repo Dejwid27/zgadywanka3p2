efaa3c63cf60bb038b309a29d19ecb727817f965
Dejwid27 <dawidpiejjo@gmail.com>
2024-09-06 10:25:14 +0200

    klawiatura

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
 
-        int wylosowanaLiczba = (int) (Math.random()*100+1);
+        int wylosowanaLiczba = (int) (Math.random() * 100 + 1);
         System.out.println(wylosowanaLiczba);
-    }
+
+
+
+
+System.out.println(wylosowanaLiczba);
+
+
+    Scanner klawiatura = new Scanner(System.in);
+    System.out.println("Podaj liczbe");
+        int wpisanaLiczba = klawiatura.nextInt();
+
+
 }
